d1beb9074132128a8e76101051b0ecbbe8311699
Terrence Sylvester <52291097+Tsylvester8@users.noreply.github.com>
2022-02-23 12:42:07 -0600

    Update calc.power.new.method.R

--- analysis/simulations/rscripts/calc.power.new.method.R
+++ analysis/simulations/rscripts/calc.power.new.method.R
@@ -5,98 +5,63 @@
 # load libraries
 library(coda)
 library(stringr)
+library(doSNOW)
+
 # get helper functions
 source("../rscripts/functions.R")
 
+# set pars for multitasking
+# define number of clusters for parallel computing
+NumberOfClusters <- 4
+cl <- makeCluster(NumberOfClusters, outfile = "")
+registerDoSNOW(cl)
 # get empirical p values
 nrep <- length(dir("../results/empP-MCMC/"))
 # get the working tree number
 wtree <- dir("../results/empP-MCMC/rep-1/")
 wtree <- as.numeric(gsub("tree-", "", wtree))
 # get number of files in a single repetition
-nfiles <- length(dir(paste("../results/empP-MCMC/rep-",
-                           sample(nrep,1),
-                           "/tree-",
-                           wtree, 
-                           sep = "")))
-# make the data table
-dat <- as.data.frame(matrix(data = NA,
-                            nrow = nrep * nfiles,
-                            ncol = 8))
-colnames(dat) <- c("run","rep","fission","fusion","aneuploidy", "tips","rr","cond")
-# fill in data table
-counter <- 1
-for(i in 1:nrep){
-  files <- dir(paste("../results/empP-MCMC/rep-",
-                     i,
-                     "/tree-",
-                     wtree, 
-                     sep = "")) 
+nfiles <- length(dir(paste("../results/empP-MCMC/rep-", sample(nrep,1), "/tree-",
+                           wtree, sep = "")))
+x <- foreach(i = 1:nrep, .verbose = T,.packages = c("stringr"))%dopar%{
+  files <- dir(paste("../results/empP-MCMC/rep-", i, "/tree-", wtree, sep = "")) 
+  # make the data table
+  dat <- as.data.frame(matrix(data = NA, nrow = nfiles, ncol = 8))
+  colnames(dat) <- c("run","rep","fission","fusion","aneuploidy", "tips","rr","cond")
   for(ii in 1:nfiles){
-    cat("working on file",counter, "out of", nrow(dat), "\n")
     # load results
-    load(paste("../results/empP-MCMC/rep-",
-               i,
-               "/tree-",
-               wtree,
-               "/",
-               files[ii],
+    load(paste("../results/empP-MCMC/rep-", i, "/tree-", wtree, "/", files[ii],
                sep = ""))
-    dat$run[counter] <- files[ii]
-    dat$rep[counter] <- i
-    dat$fission[counter] <- empP$empiricalP$EmpPvalueFission
-    dat$fusion[counter] <- empP$empiricalP$EmpPvalueFusion
-    dat$aneuploidy[counter] <- empP$empiricalP$EmpPvalueAneuploidy
-    dat$tips[counter] <- as.numeric(unlist(strsplit(str_replace_all(dat$run[counter],
-                                                                    "([A-z])",
-                                                                    replacement = ""),
-                                                    split = ".",
-                                                    fixed = T))[3])
-    dat$rr[counter] <- as.numeric(unlist(strsplit(str_replace_all(dat$run[counter],
-                                                                  "([A-z])",
-                                                                  replacement = ""),
-                                                  split = ".",
-                                                  fixed = T))[4])
-    dat$cond[counter] <- as.numeric(unlist(strsplit(str_replace_all(dat$run[counter],
-                                                                    "([A-z])",
-                                                                    replacement = ""),
-                                                    split = ".",
-                                                    
-                                                    fixed = T))[2])
-    counter <- counter + 1
-    print(counter)
+    dat$run[ii] <- files[ii]
+    dat$rep[ii] <- i
+    dat$fission[ii] <- empP$empiricalP$EmpPvalueFission
+    dat$fusion[ii] <- empP$empiricalP$EmpPvalueFusion
+    dat$aneuploidy[ii] <- empP$empiricalP$EmpPvalueAneuploidy
+    dat$tips[ii] <- as.numeric(unlist(strsplit(str_replace_all(dat$run[ii], "([A-z])", replacement = ""), split = ".",fixed = T))[3])
+    dat$rr[ii] <- as.numeric(unlist(strsplit(str_replace_all(dat$run[ii], "([A-z])", replacement = ""), split = ".", fixed = T))[4])
+    dat$cond[ii] <- as.numeric(unlist(strsplit(str_replace_all(dat$run[ii], "([A-z])", replacement = ""),split = ".", fixed = T))[2])
     # remove unwanted results
     rm(empP)
   }
+  dat
+}
+# stop cluster
+stopCluster(Cl)
+# combine results from multi threading into a single data frame
+dat <- x[[1]]
+for(i in 2:length(x)){
+  dat <- rbind(dat,x[[i]])
 }
 # sort data table by tip number
 dat <- dat[order(dat$tips),]
-
 # calculate the number of times we get a significant result
-empPower <- as.data.frame(matrix(data = NA,
-                                 nrow = nfiles,
-                                 ncol = 7))
+empPower <- as.data.frame(matrix(data = NA, nrow = nfiles, ncol = 7))
 colnames(empPower) <-  c("run", "ntip", "cond", "rr", "fission","fusion", "aneuploidy")
-
 for(i in 1:nrow(empPower)){
   empPower$run[i] <- files[i]
-  
-  empPower$ntip[i] <- as.numeric(unlist(strsplit(str_replace_all(empPower$run[i],
-                                                                 "([A-z])",
-                                                                 replacement = ""),
-                                                 split = ".",
-                                                 fixed = T))[3])
-  empPower$cond[i] <- as.numeric(unlist(strsplit(str_replace_all(empPower$run[i],
-                                                                 "([A-z])",
-                                                                 replacement = ""),
-                                                 split = ".",
-                                                 fixed = T))[2])
-  empPower$rr[i] <- as.numeric(unlist(strsplit(str_replace_all(empPower$run[i],
-                                                               "([A-z])",
-                                                               replacement = ""),
-                                               split = ".",
-                                               fixed = T))[4])
-  
+  empPower$ntip[i] <- as.numeric(unlist(strsplit(str_replace_all(empPower$run[i], "([A-z])", replacement = ""), split = ".", fixed = T))[3])
+  empPower$cond[i] <- as.numeric(unlist(strsplit(str_replace_all(empPower$run[i],"([A-z])", replacement = ""), split = ".", fixed = T))[2])
+  empPower$rr[i] <- as.numeric(unlist(strsplit(str_replace_all(empPower$run[i], "([A-z])", replacement = ""), split = ".", fixed = T))[4])
   empPower$fusion[i] <- sum(dat$fission[dat$tips == empPower$ntip[i] &  dat$cond == empPower$cond[i] & dat$rr == empPower$rr[i]] <= 0.05) / nrep
   empPower$fission[i] <- sum(dat$fusion[dat$tips == empPower$ntip[i] &  dat$cond == empPower$cond[i] & dat$rr == empPower$rr[i]] <= 0.05) / nrep
   empPower$aneuploidy[i] <- sum(dat$aneuploidy[dat$tips == empPower$ntip[i] &  dat$cond == empPower$cond[i] & dat$rr == empPower$rr[i]] <= 0.05) / nrep
@@ -104,5 +69,3 @@ for(i in 1:nrow(empPower)){
 # save data
 write.csv(dat,"../results/power.new.method.full.tab.csv", row.names = F)
 write.csv(empPower, "../results/power.new.method.csv",row.names = F)
-
-
