bf6c270221589cfa6b9dce8530aac113425b0521
Terrence Sylvester <52291097+Tsylvester8@users.noreply.github.com>
2022-02-23 14:06:42 -0600

    Update empP.all.trees.R

--- analysis/simulations/rscripts/empP.all.trees.R
+++ analysis/simulations/rscripts/empP.all.trees.R
@@ -17,59 +17,52 @@ load("../results/simData/simData.RData")
 empP <- vector(mode = "list", length = 100)
 names(empP) <- paste("tree", 1:100, sep = ".")
 # set pars
-nrep <- 100
 # get results of each condition
-for(rep in 1:100){
-  for(i in 1:length(simDat)){ # access conditions
-    for(ii in 1:length(simDat[[1]][[1]])){ # access ntips
-      for(iii in 1:length(simDat[[1]][[1]][[1]])){ # access rr
-        cond <- names(simDat)[i]
-        ntips <- names(simDat[[1]][[1]])[ii]
-        rr <- names(simDat[[1]][[1]][[1]])[iii]
-        # get the save file name
-        file.name <- paste(cond,ntips,rr,"RData",sep = ".")
-        #load results
-        load(paste("../results/simData-MCMC/",file.name, sep = ""))
-        for(iiii in 1:100){
-          # get post burnin of emperical data
-          pbrn <- getPostBurnin(results[[iiii]], burn = 0.5)
-          # run MCMC
-          empP <- getEmpiricalPMC(tree = trees[[ii]][[iiii]],
-                                  chroms = simDat[[i]][[1]][[ii]][[iii]][[iiii]],
-                                  binary = simDat[[i]][[2]][[ii]][[iii]][[iiii]],
-                                  data = pbrn,
-                                  nsim = 100,
-                                  plot.lik = F,
-                                  plot.p = F,
-                                  nclust = 100,
-                                  burn = 0.5,
-                                  args.MCMC = list(iter.temp = 20,
-                                                   iter = 100,
-                                                   prior = make.prior.exponential(r = .5),
-                                                   print.every=100))
-          if(dir.exists(paste("../results/empP-MCMC-all-trees/",rep,"/tree",iiii, sep=".")) == F){
-            dir.create(paste("../results/empP-MCMC-all-trees/",rep,"/tree",iiii, sep="."),recursive = T)  
-          }
-          save(empP, file = paste("../results/empP-MCMC-all-trees/rep",
-                                  rep,
-                                  "/tree.",
-                                  iiii,
-                                  "/empP",
-                                  file.name,
-                                  ".tree.",
-                                  iiii,
-                                  ".RData", sep = ""))
-          # remove emp from memory
-          rm(empP)
-          # free unused memory
-          gc()
+for(i in 1:length(simDat)){ # access conditions
+  for(ii in 1:length(simDat[[1]][[1]])){ # access ntips
+    for(iii in 1:length(simDat[[1]][[1]][[1]])){ # access rr
+      cond <- names(simDat)[i]
+      ntips <- names(simDat[[1]][[1]])[ii]
+      rr <- names(simDat[[1]][[1]][[1]])[iii]
+      # get the save file name
+      file.name <- paste(cond,ntips,rr,"RData",sep = ".")
+      #load results
+      load(paste("../results/simData-MCMC/",file.name, sep = ""))
+      for(iiii in 1:100){
+        # get post burn-in of empirical data
+        pbrn <- getPostBurnin(results[[iiii]], burn = 0.5)
+        # run MCMC
+        empP <- getEmpiricalPMC(tree = trees[[ii]][[iiii]],
+                                chroms = simDat[[i]][[1]][[ii]][[iii]][[iiii]],
+                                binary = simDat[[i]][[2]][[ii]][[iii]][[iiii]],
+                                data = pbrn,
+                                nsim = 100,
+                                plot.lik = F,
+                                plot.p = F,
+                                nclust = 100,
+                                burn = 0.5,
+                                args.MCMC = list(iter.temp = 20,
+                                                 iter = 100,
+                                                 prior = make.prior.exponential(r = .5),
+                                                 print.every=100))
+        if(dir.exists(paste("../results/empP-MCMC-all-trees/tree",iiii, sep=".")) == F){
+          dir.create(paste("../results/empP-MCMC-all-trees/tree",iiii, sep="."),recursive = T)  
         }
-        print(paste(file.name, "complete"))
+        save(empP, file = paste("../results/empP-MCMC-all-trees/tree.",
+                                iiii,
+                                "/empP.",
+                                file.name,
+                                sep = ""))
+        # remove emp from memory
+        rm(empP)
+        # free unused memory
+        gc()
       }
-      # remove results
-      rm(results)
-      # free unused memory
-      gc()
+      print(paste(file.name, "complete"))
     }
+    # remove results
+    rm(results)
+    # free unused memory
+    gc()
   }
 }
