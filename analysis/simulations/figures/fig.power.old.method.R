bdd0f8eb0d960bf6807e1f44f1965be1447af48e
Terrence Sylvester <52291097+Tsylvester8@users.noreply.github.com>
2022-02-23 12:58:08 -0600

    Update fig.power.old.method.R

--- analysis/simulations/figures/fig.power.old.method.R
+++ analysis/simulations/figures/fig.power.old.method.R
@@ -7,7 +7,7 @@ dat <- read.csv("../results/power.old.method.csv", as.is = T)
 
 # plot
 par(mfrow = c(3,3))
-for(i in 1:3){
+for(i in c(2,5,10)){
   # Fission
   plot(x = NULL,
        y = NULL,
@@ -15,39 +15,35 @@ for(i in 1:3){
        ylab = "Proportion of significant results",
        xlim = c(50,200),
        ylim = c(0,1))
-  lines(x = dat$tips[dat$cond == i & dat$rr == 1], y = dat$sigFission[dat$cond == i & dat$rr == 1],type = "o",col = "gray", pch = 16)
-  lines(x = dat$tips[dat$cond == i & dat$rr == 2], y = dat$sigFission[dat$cond == i & dat$rr == 2],type = "o",col = "#e66101", pch = 16)
-  lines(x = dat$tips[dat$cond == i & dat$rr == 5], y = dat$sigFission[dat$cond == i & dat$rr == 5],type = "o",col = "#fdb863", pch = 16)
-  lines(x = dat$tips[dat$cond == i & dat$rr == 10], y = dat$sigFission[dat$cond == i & dat$rr == 10],type = "o",col = "#5e3c99", pch = 16)
-  mtext(text = paste("Condition", i),side = 3,adj = 0,
+  mtext(text = paste("Rates ratio", i),side = 3,adj = 0,
         cex = 0.7)
   mtext(text = "Fission",side = 3,adj = 1,
         cex = 0.7)
-  # if(i == 1){
-  # # # add legend
-  # legend("topleft",inset=.02,
-  #        legend=c("1",
-  #                 "2",
-  #                 "5",
-  #                 "10"),
-  #        col=c("gray","#e66101" ,"#fdb863" ,"#5e3c99"),
-  #        lty=1,
-  #        cex=1,
-  #        lwd = 2,title = "Rates ratio")
-  # }
-  # Fusuin
+  # add power lines
+  lines(x = dat$tips[dat$cond == 1 & dat$rr == i], y = dat$sigFission[dat$cond == 1 & dat$rr == i],type = "o",col = "#e66101", pch = 16)
+  lines(x = dat$tips[dat$cond == 2 & dat$rr == i], y = dat$sigFission[dat$cond == 2 & dat$rr == i],type = "o",col = "#fdb863", pch = 16)
+  lines(x = dat$tips[dat$cond == 3 & dat$rr == i], y = dat$sigFission[dat$cond == 3 & dat$rr == i],type = "o",col = "#5e3c99", pch = 16)
+  # add fasle positive lines
+  lines(x = dat$tips[dat$cond == 1 & dat$rr == 1], y = dat$sigFission[dat$cond == 1 & dat$rr == 1],type = "o",col = "#e66101", pch = 16, lty = 2)
+  lines(x = dat$tips[dat$cond == 2 & dat$rr == 1], y = dat$sigFission[dat$cond == 2 & dat$rr == 1],type = "o",col = "#fdb863", pch = 16, lty = 2)
+  lines(x = dat$tips[dat$cond == 3 & dat$rr == 1], y = dat$sigFission[dat$cond == 3 & dat$rr == 1],type = "o",col = "#5e3c99", pch = 16, lty = 2)
+  # Fusion
   plot(x = NULL,
        y = NULL,
        xlab = "Number of tips",
        ylab = "Proportion of significant results",
        xlim = c(50,200),
        ylim = c(0,1))
-  lines(x = dat$tips[dat$cond == i & dat$rr == 1], y = dat$sigFusion[dat$cond == i & dat$rr == 1],type = "o", col = "gray", pch = 16)
-  lines(x = dat$tips[dat$cond == i & dat$rr == 2], y = dat$sigFusion[dat$cond == i & dat$rr == 2],type = "o", col = "#e66101", pch = 16)
-  lines(x = dat$tips[dat$cond == i & dat$rr == 5], y = dat$sigFusion[dat$cond == i & dat$rr == 5],type = "o", col = "#fdb863", pch = 16)
-  lines(x = dat$tips[dat$cond == i & dat$rr == 10], y = dat$sigFusion[dat$cond == i & dat$rr == 10],type = "o", col = "#5e3c99", pch = 16)
   mtext(text = "Fusion",side = 3,adj = 1,
         cex = 0.7)
+  # add power lines
+  lines(x = dat$tips[dat$cond == 1 & dat$rr == i], y = dat$sigFusion[dat$cond == 1 & dat$rr == i],type = "o",col = "#e66101", pch = 16)
+  lines(x = dat$tips[dat$cond == 2 & dat$rr == i], y = dat$sigFusion[dat$cond == 2 & dat$rr == i],type = "o",col = "#fdb863", pch = 16)
+  lines(x = dat$tips[dat$cond == 3 & dat$rr == i], y = dat$sigFusion[dat$cond == 3 & dat$rr == i],type = "o",col = "#5e3c99", pch = 16)
+  # add false positive lines
+  lines(x = dat$tips[dat$cond == 1 & dat$rr == 1], y = dat$sigFusion[dat$cond == 1 & dat$rr == 1],type = "o",col = "#e66101", pch = 16, lty = 2)
+  lines(x = dat$tips[dat$cond == 2 & dat$rr == 1], y = dat$sigFusion[dat$cond == 2 & dat$rr == 1],type = "o",col = "#fdb863", pch = 16, lty = 2)
+  lines(x = dat$tips[dat$cond == 3 & dat$rr == 1], y = dat$sigFusion[dat$cond == 3 & dat$rr == 1],type = "o",col = "#5e3c99", pch = 16, lty = 2)
   # Aneuploidy
   plot(x = NULL,
        y = NULL,
@@ -55,13 +51,16 @@ for(i in 1:3){
        ylab = "Proportion of significant results",
        xlim = c(50,200),
        ylim = c(0,1))
-  lines(x = dat$tips[dat$cond == i & dat$rr == 1], y = dat$sigAneuploidy[dat$cond == i & dat$rr == 1],type = "o",col = "gray", pch = 16)
-  lines(x = dat$tips[dat$cond == i & dat$rr == 2], y = dat$sigAneuploidy[dat$cond == i & dat$rr == 2],type = "o",col = "#e66101", pch = 16)
-  lines(x = dat$tips[dat$cond == i & dat$rr == 5], y = dat$sigAneuploidy[dat$cond == i & dat$rr == 5],type = "o", col = "#fdb863", pch = 16)
-  lines(x = dat$tips[dat$cond == i & dat$rr == 10], y = dat$sigAneuploidy[dat$cond == i & dat$rr == 10],type = "o",col = "#5e3c99", pch = 16)
   mtext(text = "Aneuploidy",side = 3,adj = 1,
         cex = 0.7)
-  
+  # add power lines
+  lines(x = dat$tips[dat$cond == 1 & dat$rr == i], y = dat$sigAneuploidy[dat$cond == 1 & dat$rr == i],type = "o",col = "#e66101", pch = 16)
+  lines(x = dat$tips[dat$cond == 2 & dat$rr == i], y = dat$sigAneuploidy[dat$cond == 2 & dat$rr == i],type = "o",col = "#fdb863", pch = 16)
+  lines(x = dat$tips[dat$cond == 3 & dat$rr == i], y = dat$sigAneuploidy[dat$cond == 3 & dat$rr == i],type = "o",col = "#5e3c99", pch = 16)
+  # add false positive lines
+  lines(x = dat$tips[dat$cond == 1 & dat$rr == 1], y = dat$sigAneuploidy[dat$cond == 1 & dat$rr == 1],type = "o",col = "#e66101", pch = 16, lty = 2)
+  lines(x = dat$tips[dat$cond == 2 & dat$rr == 1], y = dat$sigAneuploidy[dat$cond == 2 & dat$rr == 1],type = "o",col = "#fdb863", pch = 16, lty = 2)
+  lines(x = dat$tips[dat$cond == 3 & dat$rr == 1], y = dat$sigAneuploidy[dat$cond == 3 & dat$rr == 1],type = "o",col = "#5e3c99", pch = 16, lty = 2)
 }
 
 
