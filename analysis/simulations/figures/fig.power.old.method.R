1a11837ff5db7cf7d38e293d77412d87eb7a6a4b
Terrence Sylvester <52291097+Tsylvester8@users.noreply.github.com>
2022-02-23 12:58:01 -0600

    Update fig.power.old.method.R

--- analysis/simulations/figures/fig.power.old.method.R
+++ analysis/simulations/figures/fig.power.old.method.R
@@ -2,44 +2,9 @@
 # pradakshanas@gmail.com
 # 3rd February 2022
 
-# load libraries
-library(coda)
-# get helper functions
-source("../rscripts/functions.R")
-# list MCMC results
-files <- dir("../results/simData-MCMC/")
-files <- files[grep("cond", files)]
-# make a data frame to hold final results
-dat <- as.data.frame(matrix(data = NA,
-                            nrow = length(files),
-                            ncol = 4))
-colnames(dat) <- c("sim", "sigFission", "sigFusion", "sigAneuploidy")
-# run loop to get power at each condition
-for(i in 1:length(files)){
-  # load MCMC results
-  load(paste("../results/simData-MCMC/",files[i], sep = ""))
-  # plot likelihood
-  plotlikMCMC(data = results, burn = 0.5)
-  # get number of significant results
-  sigResults <- HPDcalc(MCMC = results,
-                        polyploidy = F,
-                        nsim = 100,
-                        plot = F,
-                        burn = 0.5)
-  # fill in the table
-  dat$sim[i] <- files[i]
-  dat$sigFission[i] <- sigResults$sigFission
-  dat$sigFusion[i] <- sigResults$sigFusion
-  dat$sigAneuploidy[i] <- sigResults$sigAneuploidy
-  # remove unwanted results
-  rm(results, pbrn, sigResults)
-}
-# make new columns to get ntip, rr and condition for plotting
-dat$tips <- rep(rep(c(100,200,50), each = 4), 3)
-dat$rr <- rep(c(1,10,2,5),9)
-dat$cond <- rep(c(1,2,3), each = 12)
-# sort data table by tip number
-dat <- dat[order(dat$tips),]
+# read data
+dat <- read.csv("../results/power.old.method.csv", as.is = T)
+
 # plot
 par(mfrow = c(3,3))
 for(i in 1:3){
